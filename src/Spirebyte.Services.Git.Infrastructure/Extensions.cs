5a0f7e82a48ea664aff28c1c433148bcb8fd82c7
Jordi Huntjens <442216@student.fontys.nl>
2022-04-19 19:56:59 +0200

    Switched to appmetrics

--- src/Spirebyte.Services.Git.Infrastructure/Extensions.cs
+++ src/Spirebyte.Services.Git.Infrastructure/Extensions.cs
@@ -69,7 +69,7 @@ public static class Extensions
             .AddMongoRepository<RepositoryDocument, string>("repositories")
             .AddWebApiSwaggerDocs()
             .AddMinio()
-            .AddPrometheus()
+            .AddMetrics()
             .AddSecurity();
     }
 
@@ -82,7 +82,7 @@ public static class Extensions
             .UseAccessTokenValidator()
             .UsePublicContracts<ContractAttribute>()
             .UseAuthentication()
-            .UsePrometheus()
+            .UseMetrics()
             .UseRabbitMq();
 
         return app;
